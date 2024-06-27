b75b87585aff8d13b800578bada101750e289e20
EmanueGCosta <emanuelgs3.abec@gmail.com>
2024-06-27 15:48:32 -0300

    Finishing bottom and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
+import Features from './components/Features'
+
+import * as Sentry from '@sentry/react'
+import Chip from './components/Chip'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -11,8 +15,11 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <Chip />
+      <Footer />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
